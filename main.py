26890f3cc33d2559c30d98d3891a1919a78b4694
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2022-08-03 15:26:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-from scipy import constants 
-print (constants.pi)
+#librerias
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+
+from glob import glob
+
+import librosa
+import librosa.display
+import IPython.display as ipd
+from itertools import cycle
+
+#cargar archivos de audio
+audio_file = glob('../Documentos/DustInTheWind/*/*.mp3')
+ipd.Audio(audio_file[0])
